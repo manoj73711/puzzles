7a4f928de7d815c0cfbde4839c1315d4351e27ff
manoj73711 <manoj73711@gmail.com>
2017-08-21 15:51:44 -0400

    Added ReadIntReturnString.java class

---
+++ src/main/java/com/manoj/puzzle/stringPuzzles/ReadIntReturnString.java
@@ -0,0 +1,16 @@
+package com.manoj.puzzle.stringPuzzles;
+
+public class ReadIntReturnString {
+
+	public String readIntReturnString(int x) {
+
+		return "" + x;
+	}
+
+	public static void main(String[] args) {
+		String m=new ReadIntReturnString().readIntReturnString(3);
+		System.out.println(m);
+
+	}
+
+}
